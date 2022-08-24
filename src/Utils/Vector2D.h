9c8ac1b7d4424b8857b8ed6ca4ace1a0d13342f6
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-08-24 19:13:39 +0200

    Add Vector2D class (not yet connected with Helix).

---
+++ src/Utils/Vector2D.h
@@ -0,0 +1,43 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+#include <utility>
+
+
+namespace Helix {
+    class Vector2D {
+        private:
+            // FIELDS
+            int x;
+            int y;
+
+        public:
+            // CONSTRUCTORS
+            Vector2D();
+            Vector2D(const int x, const int y);
+
+            // GETTERS
+            int getX() const;
+            int getY() const;
+            std::pair<int, int> getCoordinate() const;
+
+            // SETTERS
+            void setX(const int x);
+            void setY(const int y);
+            void setCoordinate(const int x, const int y);
+
+            // MEMBER OPERATORS OVERLOADING
+            Vector2D operator+(const Vector2D & vector) const;
+            Vector2D operator-(const Vector2D & vector) const;
+
+            // METHODS
+            int norm() const;
+            void normalize();
+    };
+
+    // NON-MEMBER OPERATORS OVERLOADING
+    Vector2D operator*(const int k, const Vector2D & vector); // scalar product
+    Vector2D operator-(const Vector2D & vector); // opposite
+}
+
+#endif
