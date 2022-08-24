3859fb937b7d54f14b4139764fd9c0986cdb6c4c
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-08-24 15:13:07 +0200

    Add template project to show an example of the Helix wrapper.

---
+++ src/template.cpp
@@ -0,0 +1,27 @@
+#include "Helix.h"
+#include "SDL_image.h"
+
+
+int main(int argc, char *argv[]) {
+    Helix::hlx_init("template project", 640, 480);
+
+    while (true) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                break;
+            }
+        }
+
+        Helix::setColor(0, 0, 0);
+        Helix::hlx_beginDraw();
+
+        Helix::setColor(180, 37, 98);
+        Helix::drawLine(10, 10, 630, 470);
+
+        Helix::hlx_endDraw();
+    }
+
+    Helix::hlx_quit();
+    return 0;
+}
