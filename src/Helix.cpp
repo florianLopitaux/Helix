fc774956aecc5ac25d0d4dcd10b518f646e798dc
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-08-24 14:01:26 +0200

    Repositories initialization

---
+++ src/Helix.cpp
@@ -0,0 +1,49 @@
+#include "Helix.h"
+
+#include <iostream>
+
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+
+int Helix::hlx_init(const std::string & windowName, const unsigned windowWidth, const unsigned windowHeight) {
+    // SDL initialization
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cout << "Error ! Impossible to initialize the SDL." << std::endl;
+        return -1;
+    }
+
+    // Window initialization
+    window = SDL_CreateWindow(windowName,
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              windowWidth, windowHeight,
+                              SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        std::cout << "Error ! The window cannot be created !" << std::endl
+                  << "SDL error : " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Renderer initialization
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+
+        std::cout << "Error ! The renderer cannot be created !" << std::endl
+                  << "SDL error : " << SDL_GetError() << std::endl;
+        return -1;
+
+    } else {
+        if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG)) {
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+
+            std::cout << "Error ! SDL_Image couldn't initialize !" << std::endl
+                      << "SDL_Image error : " << IMG_GetError() << std::endl;
+            return -1;
+        }
+    }
+}
---
+++ src/Helix.h
@@ -0,0 +1,13 @@
+#ifndef HELIX_H
+#define HELIX_H
+
+#include <string>
+
+#include "SDL.h"
+#include "SDL_image.h"
+
+namespace Helix {
+    int hlx_init(const std::string & windowName, const unsigned windowWidth, const unsigned windowHeight);
+}
+
+#endif
