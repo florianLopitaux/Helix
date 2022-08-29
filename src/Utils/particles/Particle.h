ad9f034cd610aa07d07a0dd6864ba0964df7ebd8
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-08-29 19:10:22 +0200

    Establishment of files structure to particle system

---
+++ src/Utils/particles/Particle.h
@@ -0,0 +1,18 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+
+namespace Helix {
+    namespace Utils {
+        class Particle {
+        private:
+            // FIELDS
+            
+        public:
+            // CONSTRUCTOR
+            Particle();
+        };   
+    }
+}
+
+#endif
---
+++ src/Utils/particles/ParticleGenerator.h
@@ -0,0 +1,27 @@
+#ifndef PARTICLE_GENERATOR_H
+#define PARTICLE_GENERATOR_H
+
+#include <vector>
+
+#include "../Vector2D.h"
+#include "Particle.h"
+
+
+namespace Helix {
+    namespace Utils {
+        class ParticleGenerator {
+            private:
+                // FIELDS
+                std::vector<Particle> particles;
+
+            public:
+                // CONSTRUCTOR
+                ParticleGenerator();
+
+                // METHODS
+                
+        };
+    }   
+}
+
+#endif
