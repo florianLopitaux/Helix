e9f50eb463d93e3df3bc4cc45a1deebf9399697c
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-09-14 11:32:09 +0200

    finishing 'Particule' class, only the 'ParticuleGenerator' class remains

--- src/Utils/particles/Particle.cpp
+++ src/Utils/particles/Particle.cpp
@@ -4,11 +4,16 @@
 
 
 // CONSTRUCTORS
-Helix::Utils::Particle::Particle(const Helix::Utils::Vector2D & position, const Helix::Utils::Vector2D & velocity,
-                                 const int life) {
+Helix::Utils::Particle::Particle(const Shape & shape,
+                                 const Helix::Utils::Vector2D & position, const Helix::Utils::Vector2D & velocity,
+                                 const int life, const unsigned size) {
+
+    this->shape = shape;
+    this->size = size;
 
     this->position = position;
     this->velocity = velocity;
+
     this->life, this->currentLife = life;
     
     SDL_Color randomColor = {};
@@ -20,15 +25,24 @@ Helix::Utils::Particle::Particle(const Helix::Utils::Vector2D & position, const 
     this->color = randomColor;
 }
 
-Helix::Utils::Particle::Particle(const Helix::Utils::Vector2D & position, const Helix::Utils::Vector2D & velocity,
-                                 const int life, const SDL_Color & color)
-    : Helix::Utils::Particle(position, velocity, life){
+Helix::Utils::Particle::Particle(const Shape & shape,
+                                 const Helix::Utils::Vector2D & position, const Helix::Utils::Vector2D & velocity,
+                                 const int life, const unsigned size, const SDL_Color & color)
+    : Helix::Utils::Particle(shape, position, velocity, life, size){
 
     this->color = color;
 }
 
 
 // GETTERS
+Helix::Utils::Shape Helix::Utils::Particle::getShape() const {
+    return this->shape;
+}
+
+unsigned Helix::Utils::Particle::getSize() const {
+    return this->size;
+}
+
 int Helix::Utils::Particle::getX() const {
     return this->position.getX();
 }
@@ -66,6 +80,16 @@ int Helix::Utils::Particle::getCurrentLife() const {
 }
 
 
+// SETTERS
+void Helix::Utils::Particle::setSize(const unsigned size) {
+    this->size = size;
+}
+
+void Helix::Utils::Particle::setColor(const SDL_Color & color) {
+    this->color = color;
+}
+
+
 // METHODS
 int Helix::Utils::Particle::update(const Uint32 & deltaTime) {
     this->position += this->velocity;
@@ -76,4 +100,27 @@ int Helix::Utils::Particle::update(const Uint32 & deltaTime) {
     }
 
     this->color.a = 255 * (this->currentLife / this->life);
+}
+
+void Helix::Utils::Particle::draw() const {
+    switch (this->shape) {
+        case Shape::EmptySquare:
+            Helix::Graphics::drawEmptyRectangle(this->position, this->size, this->size);
+            break;
+
+        case Shape::EmptyCircle:
+            Helix::Graphics::drawEmptyCircle(this->position, this->size, 0.8);
+            break;
+
+        case Shape::FillSquare:
+            Helix::Graphics::drawFillRectangle(this->position, this->size, this->size);
+            break;
+        
+        case Shape::FillCircle:
+            Helix::Graphics::drawFillCircle(this->position, this->size);
+            break;
+
+        default:
+            break;
+    }
 }
--- src/Utils/particles/Particle.h
+++ src/Utils/particles/Particle.h
@@ -7,21 +7,31 @@
 
 namespace Helix {
     namespace Utils {
+        enum class Shape {EmptySquare, EmptyCircle, FillSquare, FillCircle};
+
         class Particle {
             private:
                 // FIELDS
+                Shape shape;
+                unsigned size;
+
                 Vector2D position;
                 Vector2D velocity;
+
                 SDL_Color color;
+
                 int life;
                 int currentLife;
 
             public:
                 // CONSTRUCTORS
-                Particle(const Vector2D & position, const Vector2D & velocity, const int life);
-                Particle(const Vector2D & position, const Vector2D & velocity, const int life, const SDL_Color & color);
+                Particle(const Shape & shape, const Vector2D & position, const Vector2D & velocity, const int life, const unsigned size);
+                Particle(const Shape & shape, const Vector2D & position, const Vector2D & velocity, const int life, const unsigned size, const SDL_Color & color);
 
                 // GETTERS
+                Shape getShape() const;
+                unsigned getSize() const;
+
                 int getX() const;
                 int getY() const;
                 Vector2D getPosition() const;
@@ -31,12 +41,17 @@ namespace Helix {
                 Vector2D getVelocity() const;
 
                 SDL_Color getColor() const;
+
                 int getLifeDuree() const;
                 int getCurrentLife() const;
 
+                // SETTERS
+                void setSize(const unsigned size);
+                void setColor(const SDL_Color & color);
+
                 // METHODS
                 int update(const Uint32 & deltaTime);
-                virtual void draw() const;
+                void draw() const;
         };   
     }
 }
