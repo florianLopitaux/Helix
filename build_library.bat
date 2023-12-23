6a310a0468e7e9c20989dbc5fa58f1ae048373b7
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2023-12-23 18:08:05 +0100

    Initialized repository with SDL2 library integrated

---
+++ build_library.bat
@@ -0,0 +1,12 @@
+@echo off
+:: clean old compiled file
+IF EXIST ".\lib\libHelix.a" del /f ".\lib\libHelix.a"
+
+:: build object files
+g++ -c .\helix\src\*.cpp -std=c++17 -I .\include\ -L .\lib\ -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+
+:: build library
+ar -cqs .\lib\libHelix.a .\*.o
+
+:: clean object files
+del /f ".\*.o"
---
+++ demo/demo_v1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "SDL2/SDL.h"
+
+
+int main(int argc, char *argv[]) {
+    std::cout << "Helix project is created !" << std::endl;
+
+    return 0;
+}
